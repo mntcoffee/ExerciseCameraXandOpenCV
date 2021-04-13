17ed3547f319354ff9fe9ecdd0ed7462b794a25d
stakasaki <takasaki.shogo707@mail.kyutech.jp>
2021-04-13 15:27:24 +0900

    setup MainActivity.kt

--- app/src/main/java/com/example/exercisecameraxandopencv/MainActivity.kt
+++ app/src/main/java/com/example/exercisecameraxandopencv/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.exercisecameraxandopencv
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Executors.newSingleThreadExecutor
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,5 +27,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // カメラのパーミッションがあるならカメラを起動する．
+        // ないならリクエストを送る
+        if(allPermissionsGranted()) {
+            startCamera()
+        } else {
+            /*
+             ActivityCompat.requestPermissionsでパーミッションダイアログを表示する．
+             パーミッションダイアログの結果はonRequestPermissionsResultをオーバーライドする．
+             第1引数：Activityのインスタンス
+             第2引数：利用したいパーミッションの文字列配列
+             第3引数：リクエストコード
+             */
+            ActivityCompat.requestPermissions(
+                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // Executorのインスタンス生成(単一スレッド)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun startCamera() {}
+
+    // ユーザーがすでに特定の権限をアプリに付与しているかをチェックする．
+    // そのためにその権限をContextCompat.checkSelfPermission()メソッドに渡す．
+    // 権限がある場合PERMISSION_GRANTEDを返す．
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    // activity終了時にcameraExecutorも終了する
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
